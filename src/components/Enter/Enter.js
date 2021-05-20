e57c3025bd028a99b971db4bc7f2e97aa9c672e0
seockhun <hun00719@gmail.com>
2021-05-20 14:15:55 +0900

    [ADD] ENTERPAGE UI

--- src/components/Enter/Enter.js
+++ src/components/Enter/Enter.js
@@ -1,11 +1,29 @@
 import React from "react";
 import * as S from "./styles";
-import Hug from "../../assets/backgroundImage1.png";
+import BBImg from "../../assets/BabyBasket.png";
+import GoMainImg from "../../assets/GoMain.png";
 
 const Enter = () => {
     return (
         <S.BackWrapper>
-            <S.HugImg src={Hug}></S.HugImg>
+            <S.Title>SABY</S.Title>
+            <S.Slogan>베이비박스 지원 서비스</S.Slogan>
+            <S.ChangePage>
+                <S.InformationWrp>
+                    <S.InfShadow >
+                        <S.SeeInf>정보 보러가기</S.SeeInf>
+                        <S.HiddenInf>베이비 박스에 대해 자세한 정보를 보러가세요.</S.HiddenInf>
+                    </S.InfShadow>
+                    <S.BBInformation src={BBImg} />  {/* BB is BabyBox */}
+                </S.InformationWrp>
+                <S.MainPageWrp>
+                    <S.MainShadow>
+                        <S.SeeHome>메인페이지로</S.SeeHome>
+                        <S.HiddenHome>SABY의 메인페이지로 가세요.</S.HiddenHome>
+                    </S.MainShadow>
+                    <S.GoMain src={GoMainImg} />
+                </S.MainPageWrp>
+            </S.ChangePage>
         </S.BackWrapper>
     )
 }
--- src/components/Last.js
+++ src/components/Last.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Enter from "./Enter/Enter";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const Last = () => {
     return (
-        <Enter />
+        <Router>
+            <Route path="/first" component={Enter}></Route>
+
+        </Router>
     );
 }
 
