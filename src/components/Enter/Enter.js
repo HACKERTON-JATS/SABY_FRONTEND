e688f26a018e71b3584d1a77906abf8a3aaf5838
seockhun <hun00719@gmail.com>
2021-05-20 14:33:22 +0900

    [ADD] ENTER LINK

--- src/components/Enter/Enter.js
+++ src/components/Enter/Enter.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as S from "./styles";
 import BBImg from "../../assets/BabyBasket.png";
 import GoMainImg from "../../assets/GoMain.png";
+import { NavLink } from "react-router-dom";
 
 const Enter = () => {
     return (
@@ -10,17 +11,21 @@ const Enter = () => {
             <S.Slogan>베이비박스 지원 서비스</S.Slogan>
             <S.ChangePage>
                 <S.InformationWrp>
-                    <S.InfShadow >
-                        <S.SeeInf>정보 보러가기</S.SeeInf>
-                        <S.HiddenInf>베이비 박스에 대해 자세한 정보를 보러가세요.</S.HiddenInf>
-                    </S.InfShadow>
+                    <NavLink to="/second">
+                        <S.InfShadow >
+                            <S.SeeInf>정보 보러가기</S.SeeInf>
+                            <S.HiddenInf>베이비 박스에 대해 자세한 정보를 보러가세요.</S.HiddenInf>
+                        </S.InfShadow>
+                    </NavLink>
                     <S.BBInformation src={BBImg} />  {/* BB is BabyBox */}
                 </S.InformationWrp>
                 <S.MainPageWrp>
-                    <S.MainShadow>
-                        <S.SeeHome>메인페이지로</S.SeeHome>
-                        <S.HiddenHome>SABY의 메인페이지로 가세요.</S.HiddenHome>
-                    </S.MainShadow>
+                    <NavLink to="/third">
+                        <S.MainShadow>
+                            <S.SeeHome>메인페이지로</S.SeeHome>
+                            <S.HiddenHome>SABY의 메인페이지로 가세요.</S.HiddenHome>
+                        </S.MainShadow>
+                    </NavLink>
                     <S.GoMain src={GoMainImg} />
                 </S.MainPageWrp>
             </S.ChangePage>
