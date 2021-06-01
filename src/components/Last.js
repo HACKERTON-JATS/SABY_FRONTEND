810f882707a31dad2411d963d4c640c2b6602943
seonghyeon <shp35079@naver.com>
2021-06-01 23:04:58 +0900

    [ADD] FILE STRUCTURE
    .

--- src/components/Last.js
+++ src/components/Last.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const App = () => {
+const Last = () => {
     return (
-        <div></div>
+        <div>
+            
+        </div>
     );
 }
 
-export default App;
+export default Last;
