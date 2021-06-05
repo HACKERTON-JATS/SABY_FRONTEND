c37d5291acd80d7ee29a4f323d8c2f712e394c65
seockhun <hun00719@gmail.com>
2021-06-06 00:20:44 +0900

    [ADD] Chat UI

--- src/components/Last.js
+++ src/components/Last.js
@@ -6,6 +6,7 @@ import Signup from "./Login/Signup/Signup";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Date from "./Reservation/Content/Date";
 import Time from "./Reservation/Content/Time";
+import Chat from "./Chat/Chat";
 import WriteInfo from "./Reservation/WriteInfo/WriteInfo";
 
 const Last = () => {
@@ -15,6 +16,7 @@ const Last = () => {
             <Route exact path="/Main" component={Main}></Route>
             <Route exact path="/Signin" component={Signin}></Route>
             <Route exact path="/Signup" component={Signup}></Route>
+            <Route exact path="/Chat" component={Chat}></Route>
             <Route exact path="/Reservation-date" component={Date}></Route>
             <Route exact path="/Reservation-time" component={Time}></Route>
             <Route exact path="/Reservation-writeInfo" component={WriteInfo}></Route>
