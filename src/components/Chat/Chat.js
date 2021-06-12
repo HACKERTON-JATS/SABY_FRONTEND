4dd021245d7f3bc2d789f00af41e684bcdc71d84
seockhun <hun00719@gmail.com>
2021-06-12 11:58:39 +0900

    [ADD] question

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,9 +9,9 @@ const Chat = () => {
             <Header />
             <S.Wrapper>
                 <S.ChatBox>
-                    <S.FirstQ></S.FirstQ>
-                    <S.SecondQ></S.SecondQ>
-                    <S.ThirdQ></S.ThirdQ>
+                    <S.FirstQ>베이비박스가 정확히 무엇인가요?</S.FirstQ>
+                    <S.SecondQ>아이의 정보를 입력할 떄 정확히 무엇을 해야 하나요?</S.SecondQ>
+                    <S.ThirdQ>아이의 정보가 ㅁㄴㅇ123일ㅉ떄 어쩌라고 박상우 빨리 만들어 API</S.ThirdQ>
                 </S.ChatBox>
             </S.Wrapper>
         </>
