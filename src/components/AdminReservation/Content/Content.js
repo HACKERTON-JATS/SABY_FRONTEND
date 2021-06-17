70d5d7fee94e4b74b950e041ca1dcac909b1f69c
seockhun <hun00719@gmail.com>
2021-06-17 22:55:40 +0900

    [FIX] ADmin UI

--- src/components/AdminReservation/Content/Content.js
+++ src/components/AdminReservation/Content/Content.js
@@ -6,33 +6,33 @@ import { requestWithAccessToken } from "../../../axios/axios";
 
 const Content = () => {
     const [pageNum, setPageNum] = useState(0);
-    
+
     const getUserReservations = (e) => {
         requestWithAccessToken('GET', `/admin/reservation?page=${pageNum}`, {}, {}, 'ADMIN')
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         getUserReservations();
     }, []);
 
     return (
         <S.Wrapper>
-           <S.ReservationContainer>
+            <S.ReservationContainer>
                 <S.Date>6월 1일</S.Date>
                 <S.TextBlock>
                     <S.UserInfo>홍 정 * - 00시 ~ 01시</S.UserInfo>
                     <S.BabyInfo>
                         <S.Text>아이정보</S.Text>
-                        <S.Image src={O}/>
+                        <S.Image src={O} />
                     </S.BabyInfo>
                 </S.TextBlock>
-           </S.ReservationContainer>
+            </S.ReservationContainer>
         </S.Wrapper>
     );
 }
