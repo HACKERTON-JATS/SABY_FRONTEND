a3b987c88b71df497cb1f637674f56dce7a0043d
seonghyeon <shp35079@naver.com>
2021-06-19 13:27:02 +0900

    [ADD]AdminReservation infinite scroll

--- src/components/AdminReservation/Content/Content.js
+++ src/components/AdminReservation/Content/Content.js
@@ -1,12 +1,23 @@
-import React, { useCallback, useEffect, useState } from "react";
-import * as S from "./styles";
-import O from '../../../assets/O.png'
-import { requestWithAccessToken } from "../../../axios/axios";
+import React, { useCallback, useEffect, useState } from 'react';
+import * as S from './styles';
+import O from '../../../assets/O.png';
+import { requestWithAccessToken } from '../../../axios/axios';
 
 const Content = () => {
-    const [pageNum, setPageNum] = useState(0);
+    const [pageNum, setPageNum] = useState(-1);
+    const [maxPage, setMaxPage] = useState(0);
     const [data, setData] = useState([]);
-    const scrollEvent = ()=>{
+    const [ loading, setLoading] = useState(false);
+
+    const getUserReservations = async (page) => {
+        setPageNum(pageNum+1);
+        const res = await requestWithAccessToken('GET', `/admin/reservation?page=${page+1}`, {}, {}, 'ADMIN')
+        setData([...data, ...res.reservationInfos]);
+        setMaxPage(res.totalPages);
+        setLoading(false);
+    };
+
+    function infiniteScroll(){
         let scrollHeight = Math.max(
           document.documentElement.scrollHeight,
           document.body.scrollHeight  
@@ -16,58 +27,54 @@ const Content = () => {
           document.body.scrollTop
         );
         let clientHeight = document.documentElement.clientHeight;
-        if (scrollTop + clientHeight >= scrollHeight-200) { 
-            setPageNum(pageNum+1)
+    
+        if (Math.ceil(scrollTop + clientHeight) >= scrollHeight) {
+            setLoading(true);
         }
-    }
-    useEffect(()=>{
-        window.addEventListener("scroll",scrollEvent)
-        return () => window.removeEventListener("scroll",scrollEvent);
-    },[])
-    const getUserReservations = (e) => {
-        requestWithAccessToken('GET', `/admin/reservation?page=${pageNum}`, {}, {}, 'ADMIN')
-        .then((res) => {
-            setData([...data, ...res.reservationInfos])
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    }
-    useEffect(()=>{
-        console.log(data);
-    },[data])
-    useEffect(() =>{
-        getUserReservations();
+      }
+      useEffect(() => {
+        getUserReservations(pageNum)
+      },[]);
+      useEffect(()=>{
+        if(loading && pageNum < maxPage) getUserReservations(pageNum)
         
-    }, [pageNum]);
-    function mmddFormater(date){
-        const time=new Date(date);
-        return `${time.getMonth()+1}월 ${time.getDate()}일`
+      },[loading])
+      useEffect((e)=>{
+        window.addEventListener("scroll", infiniteScroll);
+        return () => window.removeEventListener('scroll', infiniteScroll)
+      },[])
+
+    function mmddFormater(date) {
+        const time = new Date(date);
+        return `${time.getMonth() + 1}월 ${time.getDate()}일`;
     }
-    function getHours(date){
-        const time=new Date(date);
+
+    function getHours(date) {
+        const time = new Date(date);
         return time.getHours();
     }
+
     return (
         <S.Wrapper>
-            {
-                data && data.map((i,index)=>{
-                    return(
+            {data &&
+                data.map((i, index) => {
+                    return (
                         <S.ReservationContainer key={index}>
-                            <S.Date>{mmddFormater(i.birthDate)}</S.Date>
+                            <S.Date>{mmddFormater(i.time)}</S.Date>
                             <S.TextBlock>
-                                <S.UserInfo>{i.fetusName} - {`${getHours(i.time)}시 ~ ${getHours(i.time)+1}시`}</S.UserInfo>
+                                <S.UserInfo>
+                                    {i.fetusName} - {`${getHours(i.time)}시 ~ ${getHours(i.time) + 1}시`}
+                                </S.UserInfo>
                                 <S.BabyInfo>
                                     <S.Text>아이정보</S.Text>
-                                    <S.Image src={O}/>
+                                    <S.Image src={O} />
                                 </S.BabyInfo>
                             </S.TextBlock>
                         </S.ReservationContainer>
-                    )
-                })
-            }
+                    );
+                })}
         </S.Wrapper>
     );
-}
+};
 
 export default Content;
